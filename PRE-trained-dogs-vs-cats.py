6310b5fe3667ced3aac06bd36e88bc4dcd41f90f
TomasDelaney <91500352+TomasDelaney@users.noreply.github.com>
2022-02-07 17:19:28 +0100

    Add files via upload

---
+++ PRE-trained-dogs-vs-cats.py
@@ -0,0 +1,104 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+keras = tf.keras
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# split the data manually into 80% training, 10% testing, 10% validation
+(raw_train, raw_validation, raw_test), metadata = tfds.load(
+    'cats_vs_dogs',
+    split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+    with_info=True,
+    as_supervised=True,
+)
+
+get_label_name = metadata.features['label'].int2str  # creates a function object that we can use to get labels
+
+# display 2 images from the dataset
+for image, label in raw_train.take(5):
+  plt.figure()
+  plt.imshow(image)
+  plt.title(get_label_name(label))
+
+# Data preprocessing- formatting the images
+IMG_SIZE = 160  # All images will be resized to 160x160
+
+def format_example(image, label):
+  """
+  returns an image that is reshaped to IMG_SIZE
+  """
+  image = tf.cast(image, tf.float32)
+  image = (image/127.5) - 1
+  image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+  return image, label
+
+
+train = raw_train.map(format_example)
+validation = raw_validation.map(format_example)
+test = raw_test.map(format_example)
+
+# Shuffle and batch the images
+BATCH_SIZE = 32
+SHUFFLE_BUFFER_SIZE = 1000
+
+train_batches = train.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+validation_batches = validation.batch(BATCH_SIZE)
+test_batches = test.batch(BATCH_SIZE)
+
+# Picking pretrained model
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+for image, _ in train_batches.take(1):
+   pass
+
+feature_batch = base_model(image)
+print(feature_batch.shape)
+
+# Freezing the base
+base_model.trainable = False
+
+# Adding our classifier
+prediction_layer = keras.layers.Dense(1)
+
+# Adding the model together
+model = tf.keras.Sequential([
+  base_model,
+  global_average_layer,
+  prediction_layer
+])
+
+# Training the model
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# We can evaluate the model right now to see how it does before training it on our new images
+initial_epochs = 3
+validation_steps=20
+
+loss0,accuracy0 = model.evaluate(validation_batches, steps = validation_steps)
+
+# Now we can train it on our images
+history = model.fit(train_batches,
+                    epochs=initial_epochs,
+                    validation_data=validation_batches)
+
+acc = history.history['accuracy']
+print(acc)
+
+# Saving the model
+model.save("dogs_vs_cats.h5")  # we can save the model and reload it at anytime in the future
+new_model = tf.keras.models.load_model('dogs_vs_cats.h5')
+
+
+
+
+
+
+
+
